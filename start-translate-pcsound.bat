cd2ce30bc460622625329295fc6242eec7638315
Sharrnah <55756126+Sharrnah@users.noreply.github.com>
2022-10-17 02:12:39 +0200

    Update start-translate-pcsound.bat

--- start-translate-pcsound.bat
+++ start-translate-pcsound.bat
@@ -1,1 +1,1 @@
-python audioWhisper.py --model medium --task translate --device_index 4 --phrase_time_limit 8 --pause 0.5 --websocket_ip 127.0.0.1
+python audioWhisper.py --model medium --task translate --device_index 4 --phrase_time_limit 8 --websocket_ip 127.0.0.1
