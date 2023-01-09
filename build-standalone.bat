9bd21360e385c638d66a5c7164cc2c44f52d752e
Sharrnah <55756126+Sharrnah@users.noreply.github.com>
2023-01-09 21:54:45 +0100

    [BUGFIX] Add torchaudio dependency to standalone build

--- build-standalone.bat
+++ build-standalone.bat
@@ -2,6 +2,7 @@ pip install -U pyinstaller
 pyinstaller audioWhisper.py -y ^
             --python-option=-u ^
             --hidden-import=pytorch --collect-data torch --copy-metadata torch ^
+            --hidden-import=torchaudio.lib.libtorchaudio ^
             --copy-metadata tqdm ^
             --copy-metadata regex ^
             --copy-metadata requests ^
