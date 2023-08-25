6651d1d62e57e812b5e3c4c1b80b139877d5c6ec
Sharrnah <55756126+Sharrnah@users.noreply.github.com>
2023-08-25 14:35:26 +0200

    [TASK] Add auto build files

--- dist_files/start-transcribe-mic.bat
+++
@@ -1,3 +0,0 @@
-IF EXIST %cd%\ffmpeg\bin SET PATH=%cd%\ffmpeg\bin;%PATH%
-
-audioWhisper\audioWhisper.exe --model medium --task transcribe --energy 300 --pause 1.0 --osc_ip 127.0.0.1 --websocket_ip 127.0.0.1 --phrase_time_limit 9 --condition_on_previous_text --open_browser
--- dist_files/start-translate-pcsound.bat
+++
@@ -1,3 +0,0 @@
-IF EXIST %cd%\ffmpeg\bin SET PATH=%cd%\ffmpeg\bin;%PATH%
-
-audioWhisper\audioWhisper.exe --model medium --task translate --device_index 4 --energy 300 --phrase_time_limit 12 --websocket_ip 0.0.0.0 --open_browser
