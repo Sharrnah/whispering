c734cb52edb780f216eb522efe1af4422a3bb056
Sharrnah <55756126+Sharrnah@users.noreply.github.com>
2023-09-25 21:09:37 +0200

    [FEATURE] Add Seamless M4T model as multi-modal

---
+++ Models/Singleton.py
@@ -0,0 +1,8 @@
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
