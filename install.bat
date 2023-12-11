fe8b5a483e9245daf77f6f56b8292f93fd8d2b7a
Sharrnah <55756126+Sharrnah@users.noreply.github.com>
2023-12-11 14:31:14 +0100

    [TASK] Update transformers lib
    [TASK] split pytorch requirements per GPU
    [TASK] Add triton for windows again

--- install.bat
+++ install.bat
@@ -1,2 +1,2 @@
-pip install -r requirements.txt -U --no-cache-dir
+pip install -r requirements.txt -r requirements.nvidia.txt -U --no-cache-dir
 echo "run with --force-reinstall to update also git requirements"
