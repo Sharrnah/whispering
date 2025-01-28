21c6705de391885a69096c9e6fd94709839e13f5
Sharrnah <55756126+Sharrnah@users.noreply.github.com>
2025-01-28 20:50:31 +0100

    [BUGFIX] websocket disconnect on receiving generated TTS raw audio
    This is kept for backwards compatibility of html websocket clients (now checking if its a UI connected)

--- websocket.py
+++ websocket.py
@@ -43,16 +43,21 @@ def tts_request(msgObj, websocket):
             else:
                 tts.tts.play_audio(tts_wav, settings.GetOption("device_out_index"))
         else:
-            AnswerMessage(websocket, json.dumps(
-                {"type": "tts_result", "wav_data": tts_wav.tolist(), "sample_rate": sample_rate}))
+            # send raw wav data to non UI clients (like html websocket pages, for backwards compatibility)
+            if websocket is not None and websocket != UI_CONNECTED["websocket"]:
+                AnswerMessage(websocket, json.dumps(
+                    {"type": "tts_result", "wav_data": tts_wav.tolist(), "sample_rate": sample_rate}))
             if msgObj["value"]["download"]:
                 wav_data = tts.tts.return_wav_file_binary(tts_wav)
                 if path is not None and path != '':
                     # write wav_data to file in path
-                    with open(path, "wb") as f:
-                        f.write(wav_data)
-                        AnswerMessage(websocket, json.dumps({"type": "info",
-                                                             "data": "File saved to: " + path}))
+                    try:
+                        with open(path, "wb") as f:
+                            f.write(wav_data)
+                        print("100% Finished. TTS file saved to:", path)
+                    except Exception as e:
+                        print("Failed to save TTS file:", e)
+                        traceback.print_exc()
                 else:
                     wav_data = base64.b64encode(wav_data).decode('utf-8')
                     AnswerMessage(websocket, json.dumps({"type": "tts_save", "wav_data": wav_data}))
