c8630d304827f8420aa463ab3507a90ad1fc17fb
Sharrnah <55756126+Sharrnah@users.noreply.github.com>
2025-08-09 18:18:20 +0200

    [TASK] Add call to fix possible CUDNN_STATUS_SUBLIBRARY_VERSION_MISMATCH error
    Remove nvidia-cudnn-cu12 since torch already includes cudnn.

--- install.bat
+++ install.bat
@@ -1,2 +1,5 @@
 pip install -r requirements.txt -r requirements.nvidia.txt -U --no-cache-dir --no-build-isolation
 echo "run with --force-reinstall to update also git requirements"
+
+echo "fix CUDNN_STATUS_SUBLIBRARY_VERSION_MISMATCH (uninstall version mismatch since torch already includes cudnn version)"
+pip uninstall nvidia-cudnn-cu12 -y
